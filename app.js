0a7be6a84bf530e838b0e0258a4bc9a681abdecf
agaliamor <d.shkarban01@gmail.com>
2023-01-27 08:59:56 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const { request } = require("express");
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express();
+
+
+const pageContacts = require("./view/contacts")
+const pageAnimals = require("./view/animals")
+const pageToys = require("./view/toys")
+const pageFood = require("./view/food")
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/contacts', (req, res) => {
+    console.log(req.headers)
+    res.send(pageContacts)
+})
+
+app.get('/animals', (req, res) => {
+    console.log(req.headers)
+    res.send(pageAnimals)
+})
+
+app.get('/food', (req, res) => {
+    console.log(req.headers)
+    res.send(pageFood)
+})
+
+app.get('/toys', (req, res) => {
+    console.log(req.headers)
+    res.send(pageToys)
+})
+
+app.listen(8001, () => {
+    console.log("Сервер запущен на порту 8001")
+})
---
+++ view/contacts.js
@@ -0,0 +1,21 @@
+const pageContacts = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Контакты</h1>
+  <ul>
+    <li>Буба 88005553535</li>
+    <li>Биба 88005553535</li>
+    <li>Боба 88005553535</li>
+  </ul>
+</body>
+</html>
+`
+
+module.exports = pageContacts;
